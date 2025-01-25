0a5776d9562e1fe2daa0dc82bc520015b51e22a9
AnshumanDave <87855120+AnshumanDave@users.noreply.github.com>
2025-01-25 16:17:46 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+(function () {
+    const encodedClue = "kf wzan ear vo dqri, ze jjapd lzxe. fmt zp hmk fzpap fokg hi orzvew u2hyf2nltm1memy=  ze jvafk hzosidf 64 kkmik"; //caeser wont help you (it shall assist),smthn like that might
+    
+})();
